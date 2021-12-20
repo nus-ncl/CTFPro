ede90d10a4bad244ed180ef06740be39efffa915
TunSiang <78256625+TunSiang@users.noreply.github.com>
2021-12-20 09:40:47 +0800

    Create database.py

---
+++ api/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+mysqldb://sammy:password@localhost/ncl"
+
+db_engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+
+Base = declarative_base()
+
+
+
+def get_db():
+    """
+    Function to generate db session
+    :return: Session
+    """
+    db = None
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+        
