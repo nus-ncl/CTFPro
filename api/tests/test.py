6da2677043ebd9dbe99d2693b8a26c7a382bd0ba
Aris Cahyadi Risdianto <aris.risdianto@gmail.com>
2021-12-20 16:38:28 +0800

    Fix test unit and Github action for Fast API

--- api/tests/test.py
+++ api/tests/test.py
@@ -1,29 +1,11 @@
-from multiprocessing import Process
+from fastapi.testclient import TestClient
+import sys
+sys.path.append("../")
+from apiServer import app
 
-import pytest
-import requests
-import uvicorn
-from fastapi import FastAPI
+client = TestClient(app)
 
-app = FastAPI()
-
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
-
-
-def run_server():
-    uvicorn.run(app)
-
-
-@pytest.fixture 
-def server():
-    proc = Process(target=run_server, args=(), daemon=True)
-    proc.start() 
-    yield
-    proc.kill() # Cleanup after testdef test_read_main(server):
-    response = requests.get("http://localhost:8000/")
-    assert response.status_code == 200 
-    assert response.json() == {"msg": "Hello World"}
-    
+def test_get_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Welcome!'}
