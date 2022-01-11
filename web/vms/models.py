e1cd84b6c7415aff567d26a70ff205260ce68470
Mohd Imran <45825877+Imran2923@users.noreply.github.com>
2022-01-11 16:56:46 +0800

    Update models.py

--- web/vms/models.py
+++ web/vms/models.py
@@ -8,11 +8,12 @@ from django.conf import settings
 
 
 class Component(models.Model):
-    component_id = models.IntegerField(primary_key=True)
     Type = models.CharField(max_length=45)
     hostname = models.CharField(max_length=45)
+    component_id = models.AutoField(primary_key=True)
     url_access = models.CharField(db_column='URL_access', max_length=45)  # Field name made lowercase.
     username = models.CharField(max_length=45)
+    state = models.CharField(max_length=45)
 
 
 class User(AbstractUser):
